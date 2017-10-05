6af53dbbd32674238a4b32c58603278dac977dbc
Chris Luiz <chris@chrisluiz.io>
2017-10-05 05:57:26 -0400

    functional test updates

--- functional_tests.py
+++ functional_tests.py
@@ -16,24 +16,34 @@ class NewVisitorTest(unittest.TestCase):
 
         # Notice the title page and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
-
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # You are invite to enter a to-do item right away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder',
+                                                'Enter a to do item'))
 
-# Type buy peacock feathers into atext box
+        # Type buy peacock feathers into a text box
+        inputbox.send_keys('Buy peacock feathers')
 
-# Hit enter, the page updates, adn the apge lists #1: buy peacock feathers
+        # Hit enter, the page updates, and the page lists #1: buy peacock feathers
+        inputbox.send_keys(Keys.ENTER)
 
-# There is still a text box inviting addition of another item
-# Enter, use peacock feathers to fly
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elemenets_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
 
-# The page updates and both list items are shown
+        # There is still a text box inviting addition of another item
+        # Enter, use peacock feathers to fly
+        self.fail('Finish the test')
 
-# Will the site remember the list?
-# -Explanatory text that a unique url has been generated for the list
+        # The page updates and both list items are shown
 
-# Revisit url - list still there
+        # Will the site remember the list?
+        # -Explanatory text that a unique url has been generated for the list
+
+        # Revisit url - list still there
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
