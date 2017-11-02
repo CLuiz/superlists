a331f657c963ea16f25a7aff5ac40678f95befe8
Chris Luiz <chris@chrisluiz.io>
2017-11-01 21:31:20 -0400

    simplifications

--- lists/views.py
+++ lists/views.py
@@ -3,11 +3,6 @@ from lists.models import Item
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the_only_list_in_the_world/')
-
-    items = Item.objects.all()
     return render(request, 'home.html')
 
 
